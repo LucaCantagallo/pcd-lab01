cb0b462771a12fd5dd39dc34c9727a2fb6bbdb87
DanielCapannini <daniel.capannini@studio.unibo.it>
2025-03-03 17:33:40 +0100

    bhooooooooooo

--- app/src/main/java/pcd/part1/simulation/RoadSimView.java
+++ app/src/main/java/pcd/part1/simulation/RoadSimView.java
@@ -1,9 +1,11 @@
 package pcd.part1.simulation;
 
+import akka.actor.ActorSystem;
+import pcd.part1.model.*;
 import pcd.part1.abstractSim.AbstractAgent;
 import pcd.part1.abstractSim.AbstractEnvironment;
+import pcd.part1.abstractSim.AbstractSimulation;
 import pcd.part1.abstractSim.SimulationListener;
-import pcd.part1.model.*;
 
 import javax.swing.*;
 import java.awt.*;
@@ -11,26 +13,59 @@ import java.util.List;
 
 public class RoadSimView extends JFrame implements SimulationListener {
 
-	private RoadSimViewPanel panel;
+	private final RoadSimViewPanel panel;
 	private static final int CAR_DRAW_SIZE = 10;
-	
-	public RoadSimView() {
+
+	public RoadSimView(AbstractSimulation simulation) {
 		super("RoadSim View");
-		setSize(1500,600);
-			
-		panel = new RoadSimViewPanel(1500,600); 
+		setSize(1500, 600);
+
+		panel = new RoadSimViewPanel(1500, 600);
 		panel.setSize(1500, 600);
+
+		// Pannello per i pulsanti
+		JPanel buttonPanel = new JPanel();
+		JButton startButton = new JButton("Start");
+		JButton stopButton = new JButton("Stop");
+
+		stopButton.addActionListener(e -> {
+			simulation.pause();
+		});
+
+		buttonPanel.add(startButton);
+		buttonPanel.add(stopButton);
+
+		// Pannello per il numero di passaggi
+		JPanel stepsPanel = new JPanel();
+		JLabel stepsLabel = new JLabel("Number of Steps:");
+		JTextField stepsNumber = new JTextField(10);
+		stepsPanel.add(stepsLabel);
+		stepsPanel.add(stepsNumber);
+
+		startButton.addActionListener(e -> {
+
+			String stepsText = stepsNumber.getText();
+			int steps = Integer.parseInt(stepsText);
+			if (steps > 0) {
+				simulation.elaborateSteps(steps);
+			}
+		});
+
+		JPanel controlPanel = new JPanel();
+		controlPanel.setLayout(new BorderLayout());
+		controlPanel.add(buttonPanel, BorderLayout.CENTER); // Aggiungi i pulsanti in alto
+		controlPanel.add(stepsPanel, BorderLayout.AFTER_LAST_LINE); // Aggiungi il pannello dei passaggi al centro
 
 		JPanel cp = new JPanel();
 		LayoutManager layout = new BorderLayout();
 		cp.setLayout(layout);
-		cp.add(BorderLayout.CENTER,panel);
-		setContentPane(cp);		
-		
+		cp.add(BorderLayout.CENTER, panel);
+		cp.add(BorderLayout.NORTH, controlPanel); // Aggiungi il pannello di controllo con pulsanti e passaggi
+		setContentPane(cp);
+
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-			
 	}
-	
+
 	public void display() {
 		SwingUtilities.invokeLater(() -> {
 			this.setVisible(true);
@@ -40,7 +75,7 @@ public class RoadSimView extends JFrame implements SimulationListener {
 	@Override
 	public void notifyInit(int t, List<AbstractAgent> agents, AbstractEnvironment env) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -48,32 +83,41 @@ public class RoadSimView extends JFrame implements SimulationListener {
 		var e = ((RoadsEnv) env);
 		panel.update(e.getRoads(), e.getAgentInfo(), e.getTrafficLights());
 	}
-	
-	
-	class RoadSimViewPanel extends JPanel {
-		
+
+	@Override
+	public void notifyInit(int t) {
+
+	}
+
+	@Override
+	public void notifyStepDone(int t, ActorSystem system) {
+
+	}
+
+	static class RoadSimViewPanel extends JPanel {
+
 		List<CarAgentInfo> cars;
 		List<Road> roads;
 		List<TrafficLight> sems;
-		
-		public RoadSimViewPanel(int w, int h){
+
+		public RoadSimViewPanel(int w, int h) {
 		}
 
 		public void paintComponent(Graphics g) {
-			super.paintComponent(g);   
-	        Graphics2D g2 = (Graphics2D)g;
-			g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
+			super.paintComponent(g);
+			Graphics2D g2 = (Graphics2D) g;
+			g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 			g2.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
-			g2.clearRect(0,0,this.getWidth(),this.getHeight());
-			
+			g2.clearRect(0, 0, this.getWidth(), this.getHeight());
+
 			if (roads != null) {
-				for (var r: roads) {
-					g2.drawLine((int)r.getFrom().x(), (int)r.getFrom().y(), (int)r.getTo().x(), (int)r.getTo().y());
+				for (var r : roads) {
+					g2.drawLine((int) r.getFrom().x(), (int) r.getFrom().y(), (int) r.getTo().x(), (int) r.getTo().y());
 				}
 			}
-			
+
 			if (sems != null) {
-				for (var s: sems) {
+				for (var s : sems) {
 					if (s.isGreen()) {
 						g.setColor(new Color(0, 255, 0, 255));
 					} else if (s.isRed()) {
@@ -81,29 +125,30 @@ public class RoadSimView extends JFrame implements SimulationListener {
 					} else {
 						g.setColor(new Color(255, 255, 0, 255));
 					}
-					g2.fillRect((int)(s.getPos().x()-5), (int)(s.getPos().y()-5), 10, 10);
+					g2.fillRect((int) (s.getPos().x() - 5), (int) (s.getPos().y() - 5), 10, 10);
 				}
 			}
-			
+
 			g.setColor(new Color(0, 0, 0, 255));
 
 			if (cars != null) {
-				for (var c: cars) {
+				for (var c : cars) {
 					double pos = c.getPos();
 					Road r = c.getRoad();
 					V2d dir = V2d.makeV2d(r.getFrom(), r.getTo()).getNormalized().mul(pos);
-					g2.drawOval((int)(r.getFrom().x() + dir.x() - CAR_DRAW_SIZE/2), (int)(r.getFrom().y() + dir.y() - CAR_DRAW_SIZE/2), CAR_DRAW_SIZE , CAR_DRAW_SIZE);
+					g2.drawOval((int) (r.getFrom().x() + dir.x() - CAR_DRAW_SIZE / 2),
+							(int) (r.getFrom().y() + dir.y() - CAR_DRAW_SIZE / 2), CAR_DRAW_SIZE, CAR_DRAW_SIZE);
 				}
 			}
-  	   }
-	
-	   public void update(List<Road> roads, 
-			   			  List<CarAgentInfo> cars,
-			   			List<TrafficLight> sems) {
-		   this.roads = roads;
-		   this.cars = cars;
-		   this.sems = sems;
-		   repaint();
-	   }
+		}
+
+		public void update(List<Road> roads,
+				List<CarAgentInfo> cars,
+				List<TrafficLight> sems) {
+			this.roads = roads;
+			this.cars = cars;
+			this.sems = sems;
+			repaint();
+		}
 	}
 }
