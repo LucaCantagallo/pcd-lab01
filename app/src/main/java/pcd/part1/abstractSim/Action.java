da47620fabf75b55985c25527389e102b242fbf9
DanielCapannini <daniel.capannini@studio.unibo.it>
2025-03-03 10:42:33 +0100

    copy abstract

---
+++ app/src/main/java/pcd/part1/abstractSim/Action.java
@@ -0,0 +1,7 @@
+package pcd.part1.abstractSim;
+
+/**
+ * Base type for defining actions to be executed on the environment
+ */
+public interface Action { }
+
---
+++ app/src/main/java/pcd/part1/abstractSim/Percept.java
@@ -0,0 +1,6 @@
+package pcd.part1.abstractSim;
+
+/**
+ * Base type for defining agent percepts
+ */
+public interface Percept {}
