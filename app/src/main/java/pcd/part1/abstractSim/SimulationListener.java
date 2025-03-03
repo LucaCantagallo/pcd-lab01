001203b8c3b4cd2c39d8cc0f7d5bc1fe34463cee
DanielCapannini <daniel.capannini@studio.unibo.it>
2025-03-03 17:07:50 +0100

    new SimulationListener

--- app/src/main/java/pcd/part1/abstractSim/SimulationListener.java
+++ app/src/main/java/pcd/part1/abstractSim/SimulationListener.java
@@ -1,23 +1,21 @@
 package pcd.part1.abstractSim;
 
+import akka.actor.ActorSystem;
+
 import java.util.List;
 
 public interface SimulationListener {
 
 	/**
 	 * Called at the beginning of the simulation
-	 * 
-	 * @param t
-	 * @param agents
-	 * @param env
+	 *
+	 * @param t the time of the simulation
 	 */
-	void notifyInit(int t, List<AbstractAgent> agents, AbstractEnvironment env);
-	
+	void notifyInit(int t);
+
 	/**
 	 * Called at each step, updater all updates
-	 * @param t
-	 * @param agents
-	 * @param env
+	 * @param t the time of the simulation
 	 */
-	void notifyStepDone(int t, List<AbstractAgent> agents, AbstractEnvironment env);
+	void notifyStepDone(int t, ActorSystem system);
 }
