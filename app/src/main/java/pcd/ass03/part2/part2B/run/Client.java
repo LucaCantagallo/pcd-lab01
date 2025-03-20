75ba96dc4ba712ac1415464d91fe65c5e5dcf096
DanielCapannini <daniel.capannini@studio.unibo.it>
2025-03-20 12:30:16 +0100

    bhooooo

--- app/src/main/java/pcd/ass03/part2/part2B/run/Client.java
+++ app/src/main/java/pcd/ass03/part2/part2B/run/Client.java
@@ -1,4 +1,20 @@
 package pcd.ass03.part2.part2B.run;
 
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 public class Client {
+
+    public static void main(String[] args) {
+        try {
+            // Otteniamo il registro RMI sulla porta 1099
+            Registry registry = LocateRegistry.getRegistry("localhost", 1099);
+            Server server = (Server) registry.lookup("GameServer");
+            System.out.println("ci siamo collegati al server!");
+
+            //TODO: implementare la logica del client
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
