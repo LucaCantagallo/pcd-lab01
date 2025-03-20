b2e964c513192d32765698366275f992b8a09f7c
DanielCapannini <daniel.capannini@studio.unibo.it>
2025-03-20 09:08:15 +0100

    .

--- app/src/main/java/pcd/ass03/part2/part2B/controller/GridUpdateListener.java
+++ app/src/main/java/pcd/ass03/part2/part2B/controller/GridUpdateListener.java
@@ -7,5 +7,4 @@ public interface GridUpdateListener {
     void onGridUpdated(int gridIndex);
     void onCellSelected(int gridId, int row, int col, Color color);
     void onCellUnselected(int gridId, int row, int col);
-    void onGridCompleted(int gridId, String userId);
 }
