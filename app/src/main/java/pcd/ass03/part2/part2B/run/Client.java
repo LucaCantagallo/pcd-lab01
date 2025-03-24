bfce866742c3fd30b813b371b09a632bb4e212b8
DanielCapannini <daniel.capannini@studio.unibo.it>
2025-03-24 17:56:21 +0100

    ,

--- app/src/main/java/pcd/ass03/part2/part2B/run/Client.java
+++ app/src/main/java/pcd/ass03/part2/part2B/run/Client.java
@@ -9,10 +9,15 @@ public class Client {
 
     public static void main(String[] args) {
         try {
-            RMI user1 = new RMI("2");
+            RMI user1 = new RMI("1");
             StartView view = new StartView(user1.getUsername());
             new StartController(view, user1);
             view.setVisible(true);
+
+            RMI user2 = new RMI("2");
+            StartView view2 = new StartView(user2.getUsername());
+            new StartController(view2, user2);
+            view2.setVisible(true);
 
 
         } catch (Exception e) {
